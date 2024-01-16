a0db417cb4cb48f8f006ff7670dc82630d05d4dc
Tim <118202665+MAnDArIN8001@users.noreply.github.com>
2024-01-16 17:59:12 +0300

    firtsAds

---
+++ Datastructyres/Structyres/BinaryTree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Datastructyres.Structyres {
+    public class Node<T> where T : struct, IComparable<T> {
+        private T _value;
+
+        private Node<T> _leftNode;
+        private Node<T> _rightNode;
+
+        public T value {
+            get => _value;
+            set => _value = value;
+        }
+
+        public Node<T> leftNode {
+            get => _leftNode;
+            set => _leftNode = value;
+        }
+        public Node<T> rightNode {
+            get => _rightNode;
+            set => _rightNode = value;
+        }
+
+        public Node() {
+            _value = default(T);
+            _rightNode = _leftNode = null;
+        }
+
+        public Node(T value) : base() {
+            _value = value;
+        }
+
+        ~Node() {
+            _rightNode = _leftNode = null;
+        }
+    }
+
+    internal class BinaryTree<T> where T : struct, IComparable<T> { 
+        private int _size;
+
+        private Node<T> _root;
+
+        public BinaryTree() {
+            _root = null;
+        }
+
+        ~BinaryTree() {
+            _root = null;
+        }
+
+        public void Add(T value) { 
+            if(_root is null) {
+                _root = new Node<T>(value);
+                return;
+            }
+
+            Node<T> temp = _root;
+
+            do {
+                int compareResult = value.CompareTo(temp.value);
+
+                if(temp.rightNode == null && compareResult >= 0) {
+                    temp.rightNode = new Node<T>(value);
+                    break;
+                } else if(temp.leftNode == null && compareResult == -1) {
+                    temp.leftNode = new Node<T>(value);
+                    break;
+                }
+
+                temp = compareResult == 1 ? temp.rightNode : temp.leftNode;
+            } while(true);
+        }
+
+        public bool IsExist(T value) {
+            Node<T> temp = _root;
+
+            while(temp != null) {
+                int compareResult = value.CompareTo(temp.value);
+
+                if (compareResult == 0)
+                    return true;
+
+                temp = compareResult == 1 ? temp.rightNode : temp.leftNode;
+            }
+
+            return false;
+        }
+
+        public void OutputTree() {
+            OutputNode(_root);
+        }
+
+        private void OutputNode(Node<T> node) { 
+            if(node is not null) {
+                OutputNode(node.leftNode);
+                Console.Write(node.value + " ");
+                OutputNode(node.rightNode);
+            }
+        }
+    }   
+}
---
+++ Datastructyres/Structyres/LinkedList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+
+namespace Datastructyres.Structyres {
+    public class Item<T> {
+        private T _value;
+
+        private Item<T> _next;
+
+        public T value { get => _value; set => _value = value; }
+
+        public Item<T> next { get => _next; set => _next = value; }
+
+        public Item()  {
+            _value = default(T);
+            _next = null;
+        }
+
+        public Item(T value) {
+            _value = value;
+            _next = null;
+        }
+
+        ~Item() {
+            _next = null;
+        }
+    }
+
+
+    internal class CustomLinkedList<T> {
+        private int _length;
+
+        private Item<T> _root;
+
+        public CustomLinkedList() {
+            _root = null;
+        }
+
+        ~CustomLinkedList() {
+            _root = null;
+        }
+
+        public static CustomLinkedList<T> operator + (CustomLinkedList<T> list ,T value) {
+            list.Add(value);
+
+            return list;
+        }
+
+        public T this[int index] {
+            get { 
+                if(index > _length)
+                    throw new IndexOutOfRangeException();
+
+                Item<T> response = GetItemWithIndex(index);
+
+                return response.value;
+            }
+
+            set {
+                if (index > _length)
+                    throw new IndexOutOfRangeException();
+
+                Item<T> item = GetItemWithIndex(index);
+                item.value = value;
+            }
+        }
+
+        public override string ToString() {
+            string response = "";
+
+            Item<T> temp = _root;
+
+            while (temp != null) {
+                response += temp.value + " ";
+                temp = temp.next;
+            }
+
+            return response;
+        }
+
+        public void Add(T value) {
+            _length++;
+
+            if(_root is null) {
+                _root = new Item<T>(value);
+                return;
+            }
+
+            Item<T> temp = _root;
+
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = new Item<T>(value);
+        }
+
+        public void RemoveAt(int index) { 
+            if(index > _length)
+                throw new IndexOutOfRangeException();
+
+            Item<T> lastItem = _root;
+            Item<T> preLastItem = _root;
+
+            for (int i = 0; i <= index; i++) {
+                if(i == index-1) {
+                    preLastItem = lastItem;
+                }
+
+                lastItem = lastItem.next;
+            }
+
+            preLastItem.next = lastItem;
+        }
+
+        public List<T> ToList() { 
+            List<T> response = new List<T>();
+
+            Item<T> temp = _root;
+
+            while(temp != null) {
+                response.Add(temp.value);
+                temp = temp.next;
+            }
+
+            return response;
+        }
+
+        public void Clear() {
+            _root = null;
+            _length = 0;
+        }
+
+        private Item<T> GetItemWithIndex(int index) {
+            Item<T> temp = _root;
+
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+
+            return temp;
+        }
+    }
+}
